c68b712223877624f898e7f735f75071ee9d0763
minttuNB <cerealley@gmail.com>
2019-12-07 19:52:22 +0100

    day 6 part 2

--- 6/index.js
+++ 6/index.js
@@ -1,4 +1,4 @@
-var map = {}, dist = {
+var map = {}, parents = {}, dist = {
     "COM": 0
 };
 var objectRegExp = new RegExp(/(.{1,})\)(.{1,})/);
@@ -6,11 +6,26 @@ var data = require("fs").readFileSync("data.txt", "utf8").split("\n");
 for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
     var line = data_1[_i];
     var parsed = objectRegExp.exec(line);
-    if (map["" + parsed[1]])
+    if (map["" + parsed[1]]) {
         map["" + parsed[1]].push(parsed[2]);
+        if (parents[parsed[2]]) {
+            parents[parsed[2]].push(parsed[1]);
+        }
+        else {
+            parents[parsed[2]] = [];
+            parents[parsed[2]].push(parsed[1]);
+        }
+    }
     else {
         map["" + parsed[1]] = [];
         map["" + parsed[1]].push(parsed[2]);
+        if (parents[parsed[2]]) {
+            parents[parsed[2]].push(parsed[1]);
+        }
+        else {
+            parents[parsed[2]] = [];
+            parents[parsed[2]].push(parsed[1]);
+        }
     }
 }
 function jump(cur) {
@@ -33,3 +48,29 @@ for (var i in dist) {
     sum += dist[i];
 }
 console.log(sum);
+//part 2
+var hops = 0;
+var found = false;
+var curYOU = parents["YOU"], curSAN = parents["SAN"];
+while (!found) {
+    console.log(dist[curYOU], dist[curSAN]);
+    if (dist[curYOU] > dist[curSAN]) {
+        curYOU = parents[curYOU];
+        hops++;
+        continue;
+    }
+    else if (dist[curYOU] < dist[curSAN]) {
+        curSAN = parents[curSAN];
+        hops++;
+        continue;
+    }
+    else {
+        curYOU = parents[curYOU];
+        curSAN = parents[curSAN];
+        hops += 2;
+    }
+    if (parents[curYOU] == parents[curSAN]) {
+        found = true;
+        console.log(hops);
+    }
+}
--- 6/index.ts
+++ 6/index.ts
@@ -1,4 +1,5 @@
 let map = {},
+	parents = {},
 	dist = {
 		"COM": 0
 	};
@@ -6,10 +7,26 @@ let objectRegExp = new RegExp(/(.{1,})\)(.{1,})/);
 let data = require("fs").readFileSync("data.txt", "utf8").split("\n");
 for (let line of data) {
 	let parsed = objectRegExp.exec(line);
-	if (map[`${parsed[1]}`]) map[`${parsed[1]}`].push(parsed[2]);
+	if (map[`${parsed[1]}`]){
+		map[`${parsed[1]}`].push(parsed[2]);
+		if(parents[parsed[2]]){
+			parents[parsed[2]].push(parsed[1])
+		}
+		else{
+			parents[parsed[2]] = [];
+			parents[parsed[2]].push(parsed[1])
+		}
+	} 
 	else {
 		map[`${parsed[1]}`] = [];
 		map[`${parsed[1]}`].push(parsed[2]);
+		if(parents[parsed[2]]){
+			parents[parsed[2]].push(parsed[1])
+		}
+		else{
+			parents[parsed[2]] = [];
+			parents[parsed[2]].push(parsed[1])
+		}
 	}
 }
 function jump(cur) {
@@ -28,3 +45,30 @@ for(let i in dist){
 	sum+=dist[i];
 }
 console.log(sum);
+//part 2
+let hops : number = 0;
+let found : boolean = false;
+let curYOU = parents["YOU"], 
+	curSAN = parents["SAN"];
+while(!found){
+	console.log(dist[curYOU], dist[curSAN]);
+	if(dist[curYOU] > dist[curSAN]){
+		curYOU = parents[curYOU];
+		hops++;
+		continue;
+	}
+	else if(dist[curYOU] < dist[curSAN]){
+		curSAN = parents[curSAN];
+		hops++;
+		continue;
+	}
+	else{
+		curYOU = parents[curYOU];
+		curSAN = parents[curSAN];
+		hops+=2;
+	}
+	if(parents[curYOU] == parents[curSAN]){
+		found = true;
+		console.log(hops);
+	}
+}
